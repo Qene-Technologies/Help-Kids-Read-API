4251be13864701144f97040a56e9691fda5d37e8
Henok Teklu <henokzion@gmail.com>
2017-04-30 15:04:01 +0300

    badges schema added to the kids schema

--- app_api/models/kids.js
+++ app_api/models/kids.js
@@ -13,7 +13,18 @@ var booksSchema = mongoose.Schema({
     published : {
         type : String
     }
-})
+});
+var badgesSchema = new mongoose.Schema({
+    name: {
+        type: String
+    },
+    point: {
+        type: Number,
+        min: 0,
+        max: 100
+    },
+    description: {type: String},
+});
 
 var kidsSchema = mongoose.Schema({
     name : {
@@ -29,7 +40,8 @@ var kidsSchema = mongoose.Schema({
     grade : {
         type : Number
     },
-    books_read :[booksSchema]
+    books_read :[booksSchema], 
+    badges_collected : [badgesSchema],
 });
 
 mongoose.model('Kids', kidsSchema);
